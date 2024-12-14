04f1cc500f3b4e2024ade7f251d808fc7379b3a1
Andrew <andrewlang.p@gmail.com>
2024-12-13 20:31:38 -0500

    create current user action in app to save current user session id and allow other controllers to check if user logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,21 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
 
+  def current_user
+    # Uses memoization to save info in current user before making a query
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # allows us to use @current_user in our view files
+  helper_method :current_user
+
+  # Authorize is for sending someone to the login page if they aren't logged in
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+  # With Authorize I can add the below line to any controller to restrict access unless they login
+  #before_filter :authorize
+
   private
 
   def cart
